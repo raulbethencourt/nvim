ae06315927317069a83df4ea027ad91efd0bb183
raul-bns <raul.bethencourt@bluenot-systems.com>
2025-01-24 11:05:39 +0100

    fix(cmp): add config for cmdline

--- lua/raBeta/configs/lsp/cmp.lua
+++ lua/raBeta/configs/lsp/cmp.lua
@@ -21,6 +21,27 @@ vim.keymap.set({ 'i', 's' }, '<c-l>', function()
 end, { silent = true })
 
 local cmp = require 'cmp'
+-- `/` cmdline setup.
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        {
+            name = 'cmdline',
+            option = {
+                ignore_cmds = { 'Man', '!' }
+            }
+        }
+    })
+})
 cmp.setup {
     window = {
         completion = {
