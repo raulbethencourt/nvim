6dece687b4ae54c1a0f6d893073a742f548af53f
raul-bns <raul.bethencourt@bluenot-systems.com>
2025-02-17 07:19:56 +0100

    fix(telescope): add mappings to live grep args

--- lua/raBeta/configs/telescope.lua
+++ lua/raBeta/configs/telescope.lua
@@ -1,27 +1,36 @@
-local icons = require 'icons'
+local icons = require("icons")
 local keymap = require("raBeta.utils.utils").keymap
-local action_layout = require 'telescope.actions.layout'
+local telescope = require("telescope")
+local action_layout = require("telescope.actions.layout")
+local lga_actions = require("telescope-live-grep-args.actions")
+local actions = require("telescope.actions")
+local actions_state = require("telescope.actions.state")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local sorters = require("telescope.sorters")
+local previewers = require("telescope.previewers")
+local themes = require("telescope.themes")
+local builtin = require("telescope.builtin")
+local live_grep_args_shortcuts = require('telescope-live-grep-args.shortcuts')
 
 -- NOTE: Mappings to use inside telescope
 local mappings = {
-    ['<C-c>'] = require('telescope.actions').close,
-    ['<C-u>'] = require('telescope.actions').preview_scrolling_up,
-    ['<C-d>'] = require('telescope.actions').preview_scrolling_down,
+    ['<C-c>'] = actions.close,
+    ['<C-u>'] = actions.preview_scrolling_up,
+    ['<C-d>'] = actions.preview_scrolling_down,
     ['<C-p>'] = action_layout.toggle_preview,
     ["<C-h>"] = function(prompt_bufnr)
         local opts = {}
-        local finders = require "telescope.finders"
-        local make_entry = require "telescope.make_entry"
-        local action_state = require "telescope.actions.state"
+        local action_state = actions_state
         local cmd = { "fd", "--type", "f", "--hidden", "--no-ignore" }
         local current_picker = action_state.get_current_picker(prompt_bufnr)
 
         opts.entry_maker = make_entry.gen_from_file(opts)
-        current_picker:refresh(finders.new_oneshot_job(cmd, opts), {})
+       current_picker:refresh(finders.new_oneshot_job(cmd, opts), {})
     end
 }
 
-require('telescope').setup {
+telescope.setup {
     defaults = {
         layout_config = {
             bottom_pane = {
@@ -75,9 +84,9 @@ require('telescope').setup {
         entry_prefix = '  ',
         initial_mode = 'insert',
         layout_strategy = 'horizontal_no_titles',
-        file_sorter = require('telescope.sorters').get_fuzzy_file,
+        file_sorter = sorters.get_fuzzy_file,
         file_ignore_patterns = { 'node_modules', 'vendor', 'upgrades', 'upload', 'cache' },
-        generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
+        generic_sorter = sorters.get_generic_fuzzy_sorter,
         path_display = { 'truncate' },
         preview = {
             filesize_limit = 0.1, -- MB
@@ -85,10 +94,10 @@ require('telescope').setup {
         },
         color_devicons = true,
         set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
-        file_previewer = require('telescope.previewers').vim_buffer_cat.new,
-        grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
-        qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new,
-        buffer_previewer_maker = require('telescope.previewers').buffer_previewer_maker,
+        file_previewer = previewers.vim_buffer_cat.new,
+        grep_previewer = previewers.vim_buffer_vimgrep.new,
+        qflist_previewer = previewers.vim_buffer_qflist.new,
+        buffer_previewer_maker = previewers.buffer_previewer_maker,
         mappings = {
             n = mappings,
             i = mappings,
@@ -163,9 +172,16 @@ require('telescope').setup {
         live_grep_args = {
             auto_quoting = true,
             layout_strategy = 'horizontal_no_titles',
+            mappings = {
+                i = {
+                    ["<C-k>"] = lga_actions.quote_prompt(),
+                    ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                    ["<C-f>"] = actions.to_fuzzy_refine,
+                },
+            },
         },
         ['ui-select'] = {
-            require('telescope.themes').get_dropdown {
+            themes.get_dropdown {
                 winblend = 0,
                 previewer = false,
                 layout_strategy = 'vertical_no_titles'
@@ -181,31 +197,28 @@ require('telescope').setup {
 }
 
 -- NOTE: Enable telescope extensions
-require('telescope').load_extension 'fzf'
-require('telescope').load_extension 'live_grep_args'
-require('telescope').load_extension 'ui-select'
+telescope.load_extension 'fzf'
+telescope.load_extension 'live_grep_args'
+telescope.load_extension 'ui-select'
 
 -- NOTE: Keymaps
-keymap('n', '<leader>s.', function()
-    require('telescope.builtin').find_files({ cwd = vim.fn.expand('%:p:h') })
-end, '[S]earch File in path')
-keymap('n', '<leader><space>', require('telescope.builtin').buffers, 'Buffers')
-keymap('n', '<C-s>', require('telescope.builtin').spell_suggest, '[S]pell [S]uggest')
-keymap('n', '<leader>so', "<cmd>Telescope command_history<cr>", '[S]earch command hist[O]ry')
-keymap('n', '<leader>sl', "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-    '[S]earch [L]ive Grep Args')
-keymap('n', '<leader>so', "<cmd>Telescope command_history<cr>", '[S]earch command hist[O]ry')
-keymap('n', '<leader>sf', require('telescope.builtin').find_files, '[S]earch [F]iles')
-keymap('n', '<leader>sr', require('telescope.builtin').oldfiles,
-    '[S]earch [R]ecently opened files')
-keymap('n', '<leader>sh', require('telescope.builtin').help_tags, '[S]earch [H]elp')
-keymap('n', '<leader>sR', require('telescope.builtin').registers, '[S]earch [Registers')
-keymap('n', '<leader>sw', require('telescope-live-grep-args.shortcuts').grep_word_under_cursor, '[S]earch [W]ord')
-keymap('v', '<leader>sv', require('telescope-live-grep-args.shortcuts').grep_visual_selection,
+keymap('n', '<leader><space>', builtin.buffers, 'Buffers')
+keymap('n', '<leader>ss', builtin.spell_suggest, '[S]pell [S]uggest')
+keymap('n', '<leader>sl', telescope.extensions.live_grep_args.live_grep_args, '[S]earch [L]ive Grep Args')
+keymap('n', '<leader>so', builtin.command_history, '[S]earch command hist[O]ry')
+keymap('n', '<leader>sf', builtin.find_files, '[S]earch [F]iles')
+keymap('n', '<leader>sr', builtin.oldfiles, '[S]earch [R]ecently opened files')
+keymap('n', '<leader>sh', builtin.help_tags, '[S]earch [H]elp')
+keymap('n', '<leader>sR', builtin.registers, '[S]earch [Registers')
+keymap('n', '<leader>sc', builtin.colorscheme, '[S]earch [C]olorscheme')
+keymap('n', '<leader>sk', builtin.keymaps, '[S]earch [K]eymaps')
+keymap('n', '<leader>sb', builtin.current_buffer_fuzzy_find, '[S]earch in current [B]uffer')
+keymap('n', '<leader>sw', live_grep_args_shortcuts.grep_word_under_cursor, '[S]earch [W]ord')
+keymap('v', '<leader>sv', live_grep_args_shortcuts.grep_visual_selection,
     '[S]earch [V]isual selection')
-keymap('n', '<leader>sc', require('telescope.builtin').colorscheme, '[S]earch [C]olorscheme')
-keymap('n', '<leader>sk', require('telescope.builtin').keymaps, '[S]earch [K]eymaps')
 keymap('n', '<leader>sm', function()
-    require('telescope.builtin').man_pages { sections = { 'ALL' } }
+    builtin.man_pages { sections = { 'ALL' } }
 end, '[S]earch [M]an pages')
-keymap('n', '<leader>sb', require('telescope.builtin').current_buffer_fuzzy_find, '[S]earch in current [B]uffer')
+keymap('n', '<leader>s.', function()
+    builtin.find_files({ cwd = vim.fn.expand('%:p:h') })
+end, '[S]earch File in path')
