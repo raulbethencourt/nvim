cbf32e8963f7da3106184e346edcfc6a50047672
raul-bns <raul.bethencourt@bluenot-systems.com>
2025-12-02 08:29:06 +0100

    feat(plugins): add lightbulb and opencode.nvim

--- lua/raBeta/plugins/ai.lua
+++ lua/raBeta/plugins/ai.lua
@@ -1,24 +1,41 @@
+local keymap = require('raBeta.utils.utils').keymap
+
 return {
     {
-        name = 'opencode-context',
-        'raulbethencourt/opencode-context.nvim',
-        lazy = false,
-        opts = {
-            tmux_target = nil,
-            auto_detect_pane = true,
-            auto_create_pane = true,
-            split_direction = 'vertical',
-            ui = {
-                window_type = 'float',
-                float = {
-                    width = 0.8,
-                    height = 4,
-                    margin = 2,
-                    border = 'rounded',
-                    position = 'bottom',
-                    title_pos = 'left',
+        'NickvanDyke/opencode.nvim',
+        dependencies = {},
+        config = function()
+            ---@type opencode.Opts
+            vim.g.opencode_opts = {
+                provider = {
+                    enabled = 'tmux',
+                    tmux = {},
                 },
-            },
-        },
+            }
+
+            vim.o.autoread = true
+
+            keymap({ 'n', 'x' }, '<leader>oa', function()
+                require('opencode').ask('@this: ', { submit = true })
+            end, 'Ask opencode')
+            keymap({ 'n', 'x' }, '<leader>os', function()
+                require('opencode').select()
+            end, 'Execute opencode action…')
+            keymap({ 'n', 'x' }, '<leader>op', function()
+                require('opencode').prompt '@this'
+            end, 'Add to opencode')
+            keymap({ 'n', 't' }, '<leader>ot', function()
+                require('opencode').toggle()
+            end, 'Toggle opencode')
+            keymap({ 'n', 't' }, '<leader>ol', function()
+                require('opencode').command 'session.list'
+            end, 'Toggle opencode')
+            keymap('n', '<S-C-u>', function()
+                require('opencode').command 'session.half.page.up'
+            end, 'opencode half page up')
+            keymap('n', '<S-C-d>', function()
+                require('opencode').command 'session.half.page.down'
+            end, 'opencode half page down')
+        end,
     },
 }
