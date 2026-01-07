aeb01b541e8370441d36315cc4fe853ef3e3a177
raulbethencourt <raul.bethencourt.pro@gmail.com>
2026-01-07 22:26:37 +0100

    fix(lsp): remove cmp-vsnip

--- lua/raBeta/plugins/lsp.lua
+++ lua/raBeta/plugins/lsp.lua
@@ -143,7 +143,6 @@ return {
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-nvim-lsp-signature-help',
-            'hrsh7th/cmp-vsnip',
             'onsails/lspkind.nvim',
         },
         config = function()
