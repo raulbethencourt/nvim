e621fcce098b71e25ff23e6336d127e4ecfcdbd8
raul-bns <raul.bethencourt@bluenot-systems.com>
2026-03-29 11:51:48 +0200

    fix(sql): recover floating window

--- lua/raBeta/configs/lsp/languages/sql.lua
+++ lua/raBeta/configs/lsp/languages/sql.lua
@@ -21,8 +21,19 @@ local function sqls_exec(command, arguments, range, callback)
             vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
             vim.bo[buf].filetype = 'sqls_output'
             vim.bo[buf].modifiable = false
-            local height = math.min(#lines + 1, math.floor(vim.o.lines * 0.7))
-            vim.cmd('botright ' .. height .. 'split')
+            local width = math.min(170, vim.o.columns - 4)
+            local height = math.min(#lines + 1, math.floor(vim.o.lines * 0.9))
+            vim.api.nvim_open_win(buf, true, {
+                relative = 'editor',
+                width = width,
+                height = height,
+                row = math.floor((vim.o.lines - height) / 2),
+                col = math.floor((vim.o.columns - width) / 2),
+                style = 'minimal',
+                border = 'rounded',
+                title = '',
+                title_pos = 'center',
+            })
             vim.api.nvim_win_set_buf(0, buf)
             vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = buf, silent = true })
         end
